597c37b7b407afab30fd8a642402f1b435aa68da
Álex <130467004+Alexandritta@users.noreply.github.com>
2023-07-04 17:06:05 -0300

    Add files via upload

---
+++ Terceiro Bimestre/DOM/script.js
@@ -0,0 +1,14 @@
+document.getElementById("S").style.color = "black";
+document.getElementById("S").style.fontSize = "20px";
+
+document.getElementById("A").style.color = "black";
+document.getElementById("A").style.fontSize = "20px";
+
+document.getElementById("B").style.color = "black";
+document.getElementById("B").style.fontSize = "20px";
+
+document.getElementById("C").style.color = "black";
+document.getElementById("C").style.fontSize = "20px";
+
+document.getElementById("D").style.color = "black";
+document.getElementById("D").style.fontSize = "20px";
